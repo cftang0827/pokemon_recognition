381fcc83e9c00fb34467e1613bc54ae0e75903f3
cftang <cftang@cftangde-MacBook-Pro-2.local>
2018-10-10 17:24:20 +0800

    [Modify] Modify data preprocessing

--- make_data.py
+++ make_data.py
@@ -3,21 +3,24 @@ import cv2
 import random 
 import os 
 from tqdm import tqdm 
+import pickle
 
 data_list = []
 image_dirs = os.listdir('pokemon_aug')
 
-for dd in image_dirs:
+for dd in tqdm(image_dirs):
     if dd == '.DS_Store':
         continue
     image_dir_path = os.path.join('pokemon_aug', dd, 'output')
-    for ip in tqdm(os.listdir(image_dir_path)):
+    for ip in os.listdir(image_dir_path):
         img_path = os.path.join(image_dir_path, ip)
-        print(ip)
+        # print(ip)
         if ip == '.DS_Store':
             continue
         img = cv2.imread(img_path)[:,:,::-1]
-        data_list.append(img)
+        data_list.append((dd, img))    # dd --> label. img --> image_np_data
+    
+with open('data.pickle', 'wb') as f:
+    pickle.dump(data_list, f)
 
-data_npy = np.array(data_list)
-np.save('data.npy', data_npy)
+
