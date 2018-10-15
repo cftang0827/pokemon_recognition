21ddf5ed32d238c157e8b8bd22ac7780e736cd8b
cftang <cftang@cftangde-MacBook-Pro-2.local>
2018-10-15 11:45:25 +0800

    [Modify] update train.ipynb and change image aug config

--- image_aug.py
+++ image_aug.py
@@ -4,8 +4,8 @@ import os
 for ff in os.listdir('pokemon_aug'):
     p = Augmentor.Pipeline(os.path.join('pokemon_aug', ff))
     p.rotate(probability=0.5, max_left_rotation=25, max_right_rotation=25)
-    p.zoom(probability=0.5, min_factor=0.9, max_factor=1.1)
-    # p.shear(probability=0.5, max_shear_left=10, max_shear_right=10)
+    # p.zoom(probability=0.5, min_factor=0.9, max_factor=1.1)
+    p.shear(probability=0.5, max_shear_left=10, max_shear_right=10)
     # p.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
     p.flip_left_right(probability=0.5)
     p.resize(probability=1.0, width=224, height=224)
